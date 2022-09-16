a4aee1b500a77f8e85746306b6d8f5e197f0d77c
AmeChanRain <downloadbot007@gmail.com>
2022-09-16 05:56:31 +0000

    Revert "Settings: add auto-reboot setting"
    This reverts commit 84dee9599411cb571c59cee837e9999fab54862c.

--- src/com/android/settings/security/SecuritySettings.java
+++ src/com/android/settings/security/SecuritySettings.java
@@ -105,7 +105,6 @@ public class SecuritySettings extends DashboardFragment {
         securityPreferenceControllers.add(new CombinedBiometricStatusPreferenceController(
                 context, lifecycle));
         securityPreferenceControllers.add(new ChangeScreenLockPreferenceController(context, host));
-        securityPreferenceControllers.add(new AutoRebootPreferenceController(context));
         controllers.add(new PreferenceCategoryController(context, SECURITY_CATEGORY)
                 .setChildren(securityPreferenceControllers));
         controllers.addAll(securityPreferenceControllers);
